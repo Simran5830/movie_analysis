1f8fb41b0d427cae95069e27b6e3fbb7bb6a3f91
Simran5830 <55655798+Simran5830@users.noreply.github.com>
2021-04-10 20:28:55 +0530

    Update tidy.R

--- tidy.R
+++ tidy.R
@@ -1,28 +1,33 @@
 library(dplyr)
 library(tidyr)
+library(tidyverse)
 mov<-read.csv("C:/Users/Simran/Desktop/movie_analysis/MoviesOnStreamingPlatforms.csv")
 str(mov)
 
 table (complete.cases(mov))
 summary  (mov [!complete.cases(mov),])
 
-mov<-mov %>%
-  pivot_longer(cols = c(`ID`, `Year`), names_to = "Number", values_to = "ReleaseYear")
-mov
-mov %>%
-  pivot_wider(names_from =Title , values_from = IMDb)
+From the dataset,it is easy to infer that some cleaning need to be done before we can decipher information.Lets do it first.
 
+*Changing Rotten.Tomatoes to Rotten_Tomatoes*
+```{r}
+mov<-mov%>% 
+  setNames(names(.)%>% 
+             str_replace_all("\\.","_"))
+```
 
+*Removing unnecessary columns*
+
+```{r}
+mov<-mov[-c(7:11)]  
+summary(mov)
+```
+*Removing NA values*
+
+```{r}
 mov<-mov %>%
-  separate(
-    col = Genres,
-    into = c("Action", "Adventure"),
-    convert = TRUE
-  )
-mov
-mov<-mov %>% 
-  unite(col = "Genre", Action,Adventure)
-mov
+  drop_na()
+```
 
 
 write.csv(mov, file = "C:/Users/Simran/Desktop/movie_analysis/TidyMovie.csv")
